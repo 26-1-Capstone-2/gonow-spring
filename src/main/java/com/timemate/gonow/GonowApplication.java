bea0ecd9795801efdf04f04b94f0d0270bfb1d2e
youngwoong9 <youngwoong9@naver.com>
2026-05-02 02:46:29 +0900

    초기 프로젝트 세팅

---
+++ src/main/java/com/timemate/gonow/GonowApplication.java
@@ -0,0 +1,13 @@
+package com.timemate.gonow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GonowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GonowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/timemate/gonow/GonowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timemate.gonow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GonowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
